4281d316d7f0b441630b6562f8cdee59d1532c36
MariaGabriellaCardosodepaula <160916450+MariaGabriellaCardosodepaula@users.noreply.github.com>
2025-05-08 16:17:40 -0300

    Primeiro Commit

---
+++ scripts/CanvasLayer.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+@onready var rotuloMoedas = $Label
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if %GameManager:
+		rotuloMoedas.text = str(%GameManager.score, " / ", %GameManager.objetivo)
+	
+	
+
+
+func _on_button_pressed() -> void:
+	get_tree().paused = true
+	$Pause_menu.visible = true
+	print("TESTE")
+	pass # Replace with function body.
---
+++ scripts/globais.gd
@@ -0,0 +1,4 @@
+extends Node
+
+
+var estado_morte = false
